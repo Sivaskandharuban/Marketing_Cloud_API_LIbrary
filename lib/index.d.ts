a55d7db513f413095d3b48165589bd03c0812c91
“Sivaskandha <sivaskandharuban@gmail.com>
2021-09-21 18:37:15 +0530

    version patch 12

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export default class mcGenericMethods {
     getOAuthAccessToken(clientId: any, clientSecret: any, grantType: string, code: any, redirect_uri: any): Promise<any>;
     getRefreshToken(refreshToken: string, tssd: string, clientId: any, clientSecret: any): Promise<any>;
+    getSenderDomain(mcVals: any): Promise<any>;
 }
